2b19e58ba31fa0da4a963bb75bcbc5ffc2f528d6
macleanpinto <macleanmauricepinto@gmail.com>
2019-03-22 06:06:01 +0000

    Improved Solution Problem11

--- src/main/java/com/alpha/problemsolving/DailyCodingProblem11.java
+++ src/main/java/com/alpha/problemsolving/DailyCodingProblem11.java
@@ -1,7 +1,9 @@
 package com.alpha.problemsolving;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.TreeMap;
 
 /*
 This problem was asked by Twitter.
@@ -15,13 +17,18 @@ class DailyCodingProble11 {
 
     public static void main(String args[]) {
 
-        String[] words = { "dog", "deer", "deal" };
+        String[] words = { "dog", "dee", "deer", "deal" };
         Trie trie = new Trie();
         for (String word : words) {
             trie.insert(word);
         }
-        trie.search("de", trie.root, 0).stream().forEach(word -> System.out.println(word));
-        trie.search("do", trie.root, 0).stream().forEach(word -> System.out.println(word));
+        trie.search("de", trie.root, 0).forEach(word -> System.out.println(word));
+        trie.search("do", trie.root, 0).forEach(word -> System.out.println(word));
+
+        Trie2 trie2 = new Trie2(Arrays.asList(words));
+        trie2.search("de").forEach(word -> System.out.println(word));
+        trie2.search("do").forEach(word -> System.out.println(word));
+
     }
 }
 
@@ -74,10 +81,72 @@ class Trie {
         for (TrieNode child : currentNode.children) {
             if (child != null) {
                 if (child.isWord == true) {
-                    results.add(sb.append(child.content).toString());
+                    results.add(new StringBuilder(sb).append(child.content).toString());
                 }
                 findWords(child, new StringBuilder(sb).append(child.content), results);
             }
         }
+    }
+}
+
+// Imporved solution
+class Trie2 {
+    private class TrieNode2 extends TreeMap<Character, TrieNode2> {
+        public static final long serialVersionUID = 1L;
+        public boolean isWord;
+    }
+
+    private TrieNode2 root;
+
+    public Trie2(List<String> words) {
+        this();
+        words.forEach(this::insert);
+    }
+
+    public Trie2() {
+        root = new TrieNode2();
+    }
+
+    public void insert(String word) {
+        TrieNode2 current = root;
+        for (int i = 0; i < word.length(); i++) {
+            current = current.computeIfAbsent(word.charAt(i), k -> new TrieNode2());
+        }
+        current.isWord = true;
+    }
+
+    public List<String> search(String word) {
+        List<String> results = new ArrayList<>();
+
+        TrieNode2 node = findNode(word, root, 0);
+        if (node == null) {
+            return results;
+        }
+
+        findWords(node, new StringBuilder(word), results);
+        return results;
+    }
+
+    private TrieNode2 findNode(String word, TrieNode2 current, int index) {
+        if (index == word.length()) {
+            return current;
+        }
+
+        Character ch = word.charAt(index);
+        if (!current.containsKey(ch)) {
+            return null;
+        }
+
+        return findNode(word, current.get(ch), ++index);
+    }
+
+    private void findWords(TrieNode2 current, StringBuilder sb, List<String> results) {
+        current.forEach((Character ch, TrieNode2 child) -> {
+            StringBuilder word = new StringBuilder(sb).append(ch);
+            if (child.isWord) {
+                results.add(word.toString());
+            }
+            findWords(child, word, results);
+        });
     }
 }
