500d5723d555b2472137179108b002323225c3cb
macleanpinto <macleanmauricepinto@gmail.com>
2019-03-22 05:15:28 +0000

    Problem12

--- src/main/java/com/alpha/problemsolving/DailyCodingProblem12.java
+++ src/main/java/com/alpha/problemsolving/DailyCodingProblem12.java
@@ -14,26 +14,42 @@ For example, if N is 4, then there are 5 unique ways:
     2, 2
 
 What if, instead of being able to climb 1 or 2 steps at a time, you could climb any number from a set of positive integers X? For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time.
+
+Explanation : https://codereview.stackexchange.com/questions/215973
 */
 public class DailyCodingProblem12 {
     public static void main(String args[]) {
         int[] X = { 1, 2 };
         int N = 4;
-        int[] memory = new int[N];
-        memory[0] = 1;
-        memory[1] = 1;
-        int result = noOfWays(N, X, memory);
+        int result = solution(N, X);
+        System.out.println(result);
+
+        int[] X1 = { 1, 2, 3 };
+        N = 4;
+        result = solution(N, X1);
+        System.out.println(result);
+
+        int[] X2 = { 1, 2, 3 };
+        N = 3;
+        result = solution(N, X2);
         System.out.println(result);
     }
 
+    static int solution(int N, int[] X) {
+        int[] memory = new int[N + 1];
+        memory[0] = 1;
+        memory[1] = 1;
+        return noOfWays(N, X, memory);
+    }
+
     static int noOfWays(int N, int[] X, int[] memory) {
-        if (memory[N] == 0) {
+        if (memory[N] != 0) {
             return memory[N];
         }
         int noOfWays = 0;
-        for (int i = 0; i < X.length; i++) {
-            memory[N-X[i]]= noOfWays(N - X[i], X,memory);
-            noOfWays+=memory[N-X[i]];
+        for (int i = 0; i < X.length && (N - X[i] >= 0); i++) {
+            memory[N - X[i]] = noOfWays(N - X[i], X, memory);
+            noOfWays += memory[N - X[i]];
         }
         return noOfWays;
 
