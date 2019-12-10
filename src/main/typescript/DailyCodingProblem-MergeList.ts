7dd59624c62b08f2781c0a4726ca79ad95262367
macleanpinto <macleanmauricepinto@gmail.com>
2019-12-10 16:53:48 +0000

    Merge K list

---
+++ src/main/typescript/DailyCodingProblem-MergeList.ts
@@ -0,0 +1,13 @@
+/*  Return a new sorted merged list from K sorted lists, each with size N. */
+let arr1 = [1, 3, 6];
+let arr2 = [2, 4, 7];
+let arr3 = [5, 8, 9];
+
+let list = [arr1, arr2, arr3];
+function mergedList(List: number[][]) {
+    var arr = Array(list.length);
+    console.log(arr);
+}
+
+
+mergedList(list);
