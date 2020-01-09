0b942083ed6360bb84928cde576def82e5c04cd3
macleanpinto <macleanmauricepinto@gmail.com>
2020-01-09 17:17:56 +0000

    DailyCodingProblem13

---
+++ src/main/typescript/DailyCodingProblem23.ts
@@ -0,0 +1,67 @@
+/*
+This problem was asked by Google.
+
+You are given an M by N matrix consisting of booleans that represents a board. Each True boolean represents a wall. Each False boolean represents a tile you can walk on.
+
+Given this matrix, a start coordinate, and an end coordinate, return the minimum number of steps required to reach the end coordinate from the start. If there is no possible path, then return null. You can move up, left, down, and right. You cannot move through walls. You cannot wrap around the edges of the board.
+
+For example, given the following board:
+
+[[f, f, f, f],
+[t, t, f, t],
+[f, f, f, f],
+[f, f, f, f]]
+
+and start = (3, 0) (bottom left) and end = (0, 0) (top left), the minimum number of steps required to reach the end is 7, since we would need to go through (1, 2) because there is a wall everywhere else on the second row.
+*/
+
+class Coordinate {
+    private _x: number;
+    private _y: number;
+    constructor(x: number, y: number) {
+        this._x = x;
+        this._y = y;
+    }
+    public get x() {
+        return this.x;
+    }
+    public get y() {
+        return this.y;
+    }
+}
+
+function walkable(row: number, col: number, board: [][]) {
+    if (row < 0 || row > board[0].length) {
+        return false;
+    }
+    if (col < 0 || col > board[0].length) {
+        return false;
+    }
+    return board[row][col] === 'f' ? false : true;
+
+}
+
+function get_walkable_neighbour(row: number, col: number, board: [][]) {
+
+    let neighbours = [
+        new Coordinate(row, col - 1),
+        new Coordinate(row, col + 1),
+        new Coordinate(row - 1, col),
+        new Coordinate(row + 1, col)];
+    return neighbours.map((cord: Coordinate) => {
+        if (walkable(cord.x, cord.y, board)) {
+            return cord;
+        }
+    });
+
+}
+
+function minStepsRequired(start: Coordinate, end: Coordinate) {
+    let visited: Map<Coordinate, boolean> = new Map();
+    let board =
+        [['f', 'f', 'f', 'f'],
+        ['t', 't', 'f', 't'],
+        ['f', 'f', 'f', 'f'],
+        ['f', 'f', 'f', 'f']];
+
+}
