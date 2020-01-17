91c25e68efa3cf743a22332489200d252be483e4
macleanpinto <macleanmauricepinto@gmail.com>
2020-01-17 17:41:01 +0000

    DailyCodingProblem25

--- src/main/typescript/DailyCodingProblem25.ts
+++ src/main/typescript/DailyCodingProblem25.ts
@@ -14,15 +14,44 @@ For example, given the regular expression "ra." and the string "ray", your funct
 Given the regular expression ".*at" and the string "chat", your function should return true. The same regular expression on the string "chats" should return false.
 */
 
-function validate(str: string, regex: string) {
-
-    if (regex.length === 0) {
-        return str.length == 0 ? true : false;
+function matches_first_char(str: string, regex: String) {
+    if (str.length != 0 && (regex[0] === '.' || regex[0] === str[0])) {
+        return true;
+    } else {
+        return false;
     }
-    if (regex[0] === '.' || regex[0] === str[0]) {
-        return validate(str.slice(1, str.length), regex.slice(1, regex.length));
-    }
-    return validate(str, regex.slice(1, regex.length)) || validate(str.slice(1, str.length), regex.slice(1, regex.length));
 }
-console.log(validate('chat', '.*at'));
-console.log(validate('chats', '.*at'));
+function matches(str: string, regex: string) {
+    if (regex == '') {
+        return str === '';
+    }
+    if (regex.length === 1 || regex[1] !== '*') {
+        if (matches_first_char(str, regex)) {
+            return matches(str.slice(1,str.length), regex.slice(1,regex.length));
+        } else {
+            return false;
+        }
+
+    }
+    else {
+        if (matches(str, regex.slice(2, regex.length))) {
+            return true;
+        }
+        let i = 0;
+        while (matches_first_char(str.slice(i, str.length), regex)) {
+            if (matches(str.slice(i + 1, str.length), regex)) {
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+
+}
+console.log(matches('chat', '.*at'));
+console.log(matches('chats', '.*at'));
+console.log(matches('cat', '.*at'));
+console.log(matches('ccat', '.*at'));
+console.log(matches('ccat', 'd*at'));
+console.log(matches('ccat', 'c.*at'));
+console.log(matches('ccat', 'cd*at'));
